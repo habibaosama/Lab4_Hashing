0e1f4c8514e9aa11ca1e8e18f2b364ef64fedd06
Habiba <habebaosama14@gmail.com>
2022-05-11 23:31:08 +0200

    hashfunction

--- src/Hashing.java
+++ src/Hashing.java
@@ -6,13 +6,14 @@ public class Hashing {
     public int b;
     public int n;
     public int[] S;
-    public LinkedList<int[][]> hash;
+    //for saving the random H
+    public LinkedList<int[][]> hashRandomized;
 
     Hashing(int[] a) {
         n = a.length;
         b = (int) Math.ceil(Math.log(n) / Math.log(2));
         S = a;
-        hash = new LinkedList<>();
+        hashRandomized = new LinkedList<>();
         hashFunction();
     }
 
@@ -31,9 +32,40 @@ public class Hashing {
 
     //will return the index
     public void hashFunction() {
-       
+       int [][]H=randomH();
+        hashRandomized.add(H);
+       for (int i=0;i<n;i++){
+           int []x=convertToBinary(S[i]);
+           int [] index=multiply(H,x);
+       }
     }
+    public int[] multiply(int[][] H,int []x){
+        int []multiply=new int[H.length];
+         int sum=0;
+        for(int i=0;i<H.length;i++){
+           sum=0;
+            for(int j=0;j<H[0].length;j++){
+               sum+=H[i][j]*x[j];
+            }
+            if(sum%2==0)
+                multiply[i]=0;
+            else
+                multiply[i]=1;
 
+        }
+       return multiply;
+    }
+   public int[] convertToBinary(int num){
+          //since keys will all be 32-bit integers
+       int []bin=new int[32];
+       int i=0;
+       while(num!=0){
+           bin[i++]=num%2;
+           num/=2;
+       }
+       return bin;
+
+   }
 
 }
 //expected number of collision =n/m
