4e8f35a3e1420c2ef81fda46e4187b7d3104be43
MaiAhmedHussein <mayoyaahmed66@gmail.com>
2022-05-12 13:32:42 +0200

    O(N) Added:))

--- src/On.java
+++ src/On.java
@@ -12,28 +12,29 @@ public class On {
     private int[]result;
     //for saving the random H
     public LinkedList<int[][]> hashRandomized;
-
-    public  On(int[] a) {
-        n = a.length;
+    Hashing hashing;
+    public  On(Hashing hashingObj) {
+        this.hashing = hashingObj;
+        n = hashing.n;
         b = (int) Math.ceil(Math.log(n) / Math.log(2));
-        S = a;
         hashRandomized = new LinkedList<>();
-        exist=new boolean[n*n];
-        result=new int [n*n];
+        exist=new boolean[n];
+        result=new int [n];
         hashFunction();
     }
     /////////////////////////////////////////////////////////////////////////////////////////////////
     public void hashFunction() {
+        int[]S=hashing.S;
         ArrayList<Integer>[]  h=new ArrayList[n];
         for (int i = 0; i < n; i++) {
             h[i] = new ArrayList<Integer>();
         }
-        int[][] H = randomH();
+        int[][] H = hashing.randomH(b);
         hashRandomized.add(H);
         for (int i = 0; i < n; i++) {
-            int[] x = convertToBinary(S[i]);
-            int[] indexBinary = multiply(H, x);
-            int index=convertToDecimal(indexBinary);
+            int[] x = hashing.convertToBinary(S[i]);
+            int[] indexBinary = hashing.multiply(H, x);
+            int index=hashing.convertToDecimal(indexBinary);
             if(exist[index]){
                 //the same index exist
                 //collision ->so call again random the H and get another index
@@ -47,65 +48,12 @@ public class On {
         }
     }
 
-    private int[][] randomH() {
-        int[][] H = new int[b][U];
-        Random random = new Random();
-        for (int i = 0; i < b; i++) {
-            for (int j = 0; j < U; j++) {
-                H[i][j] = random.nextInt(2);
-            }
-        }
-       /* System.out.println("H");
-        for(int i=0;i<b;i++){
-            for(int j=0;j<U;j++){
-                System.out.print(H[i][j]+" ");
-            }
-            System.out.println();
-        }*/
-        return H;
-    }
-    private int convertToDecimal(int[] bin){
-        int val=0;
-        for(int i=0;i < bin.length;i++)
-            val+=bin[i]*Math.pow(2,i);
-        return val;
-    }
-    public int[] multiply(int[][] H, int[] x) {
-        int[] multiply = new int[H.length];
-        int sum = 0;
-        for (int i = 0; i < H.length; i++) {
-            sum = 0;
-            for (int j = 0; j < H[0].length; j++) {
-                sum += H[i][j] * x[j];
-            }
-            if (sum % 2 == 0)
-                multiply[i] = 0;
-            else
-                multiply[i] = 1;
 
-        }
-      /* System.out.println("multiply");
-        for(int i=0;i< H.length;i++)
-            System.out.print(multiply[i] +" ");*/
 
-        return multiply;
-    }
-    public int[] convertToBinary(int num) {
-        //since keys will all be 32-bit integers
-        System.out.println("binary " + num);
-        int[] bin = new int[32];
-        int i = 0;
-        while (num != 0) {
-            bin[i++] = num % 2;
-            num /= 2;
-        }
-       /* for(int j=0;j<i;j++ )
-              System.out.print(bin[j]+ " ");
-        System.out.println();*/
-        return bin;
-    }
+
+
     public void print(){
-        for(int i=0;i<n*n;i++)
+        for(int i=0;i<n;i++)
             System.out.println(result[i]);
     }
 
